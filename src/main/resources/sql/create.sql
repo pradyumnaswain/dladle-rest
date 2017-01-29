a73a3d1f1d7b5f075a743aa4f34b020a6e6b2b7e
Jugal Mandal <jmandal@sysappweb.in>
2017-01-29 17:04:13 +0530

    cleaned up DB tables

--- src/main/resources/sql/create.sql
+++ src/main/resources/sql/create.sql
@@ -10,12 +10,56 @@ GRANT ALL PRIVILEGES ON DATABASE dladle TO sysadmin;
 
 DROP OWNED BY sysadmin;
 
+DROP DATABASE dladle;
+
+/*User Type*/
 CREATE TABLE user_type
 (
   id   SERIAL PRIMARY KEY  NOT NULL,
   name VARCHAR(128)        NOT NULL
 );
-/*user_dladle*/
+
+/*service_type*/
+CREATE TABLE service_type
+(
+  id   SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(128)       NOT NULL
+);
+
+/*amenities_type*/
+CREATE TABLE amenities_type
+(
+  id   SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(128)       NOT NULL
+);
+/*place_type*/
+CREATE TABLE place_type
+(
+  id   SERIAL PRIMARY KEY,
+  name VARCHAR(20)
+);
+/*bedroom_type*/
+CREATE TABLE bedroom_type
+(
+  id   SERIAL PRIMARY KEY,
+  name VARCHAR(20)
+);
+
+/*Home View Type*/
+CREATE TABLE home_view_type
+(
+  id   SERIAL PRIMARY KEY,
+  name VARCHAR(20)
+);
+
+/*Years of Experience*/
+CREATE TABLE years_exp
+(
+  id   SERIAL PRIMARY KEY,
+  name VARCHAR(20)
+);
+
+/* User*/
 CREATE TABLE user_dladle
 (
   id                SERIAL PRIMARY KEY  NOT NULL,
@@ -24,90 +68,63 @@ CREATE TABLE user_dladle
   user_type_id      INT,
   verified          BOOLEAN,
   verification_code VARCHAR(100),
-  otp               VARCHAR(10)
+  otp               INT,
+  first_name        VARCHAR(20),
+  last_name         VARCHAR(20),
+  id_number         VARCHAR(45),
+  FOREIGN KEY (user_type_id) REFERENCES user_type (id)
 );
+CREATE UNIQUE INDEX user_dladle_email_uindex
+  ON user_dladle (emailid);
 /*landlord*/
 CREATE TABLE landlord
 (
-  id              SERIAL PRIMARY KEY NOT NULL,
-  first_name      VARCHAR(20),
-  last_name       VARCHAR(20),
-  identity_number VARCHAR(45),
-  user_id         INT
+  id      SERIAL PRIMARY KEY NOT NULL,
+  user_id INT,
+  cell_number INT,
+  FOREIGN KEY (user_id) REFERENCES user_dladle (id)
 );
-/*tenant*/
-CREATE TABLE tenant
+
+/*property*/
+CREATE TABLE property
 (
-  id              SERIAL PRIMARY KEY NOT NULL,
-  first_name      VARCHAR(20),
-  last_name       VARCHAR(20),
-  identity_number VARCHAR(45),
-  user_id         INT,
-  landlord_id     INT,
-  house_id        INT
+  id                SERIAL PRIMARY KEY NOT NULL,
+  landlord_id       INT,
+  location          VARCHAR(100),
+  address           VARCHAR(100),
+  place_name        VARCHAR(100),
+  place_type_id     INT,
+  complex_name      VARCHAR(45),
+  unit_number       VARCHAR(20),
+  bedroom_type_id   INT,
+  image_url         VARCHAR(100),
+  home_view_type_id INT,
+  FOREIGN KEY (place_type_id) REFERENCES place_type (id),
+  FOREIGN KEY (bedroom_type_id) REFERENCES bedroom_type (id),
+  FOREIGN KEY (home_view_type_id) REFERENCES home_view_type (id),
+  FOREIGN KEY (landlord_id) REFERENCES landlord (id)
 );
-/*tenant*/
-CREATE TABLE vendor
+
+/*house*/
+CREATE TABLE house
 (
-  id               SERIAL PRIMARY KEY NOT NULL,
-  business_name    VARCHAR(20),
-  user_id          INT,
-  business_address VARCHAR(50),
-  service_type_id  INT
+  id           SERIAL PRIMARY KEY NOT NULL,
+  house_number INT UNIQUE         NOT NULL,
+  property_id  INT,
+  FOREIGN KEY (property_id) REFERENCES property (id)
 );
-/*service_type*/
-CREATE TABLE service_type
-(
-  id   SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(128)       NOT NULL
-);
-/*service*/
-CREATE TABLE service
-(
-  id              SERIAL PRIMARY KEY NOT NULL,
-  service_type_id INT,
-  vendor_id       INT
-);
-/*amenities_type*/
-CREATE TABLE amenities_type
-(
-  id   SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(128)       NOT NULL
-);
+
 /*amenities*/
 CREATE TABLE amenities
 (
   id                SERIAL PRIMARY KEY NOT NULL,
   amenities_type_id INT,
   count             INT,
-  house_id          INT
+  house_id          INT,
+  FOREIGN KEY (amenities_type_id) REFERENCES amenities_type (id),
+  FOREIGN KEY (house_id) REFERENCES house (id)
 );
-/*house*/
-CREATE TABLE house
-(
-  id           SERIAL PRIMARY KEY NOT NULL,
-  house_number INT UNIQUE         NOT NULL,
-  property_id  INT
-);
-/*property*/
-CREATE TABLE property
-(
-  id          SERIAL PRIMARY KEY NOT NULL,
-  landlord_id INT,
-  location    VARCHAR(100),
-  address     VARCHAR(100),
-  place_name  VARCHAR(100)
-);
-/*lease*/
-CREATE TABLE lease
-(
-  id           SERIAL PRIMARY KEY NOT NULL,
-  landlord_id  INT,
-  tenant_id    INT,
-  lease_date   DATE,
-  renewal_date DATE,
-  remark       VARCHAR(100)
-);
+
 /*rating*/
 CREATE TABLE rating
 (
@@ -122,64 +139,56 @@ CREATE TABLE rating_comments
   id          SERIAL PRIMARY KEY NOT NULL,
   rating_id   INT,
   user_id     INT,
-  description VARCHAR(100)
+  description VARCHAR(100),
+  FOREIGN KEY (rating_id) REFERENCES rating (id)
 );
 
-CREATE TABLE place_type
+/*tenant*/
+CREATE TABLE tenant
 (
-  id   SERIAL PRIMARY KEY,
-  name VARCHAR(20)
+  id          SERIAL PRIMARY KEY NOT NULL,
+  user_id     INT,
+  landlord_id INT,
+  house_id    INT,
+  FOREIGN KEY (user_id) REFERENCES user_dladle (id),
+  FOREIGN KEY (landlord_id) REFERENCES landlord (id),
+  FOREIGN KEY (house_id) REFERENCES house (id)
 );
-CREATE TABLE bedroom_type
+/*tenant*/
+CREATE TABLE vendor
 (
-  id   SERIAL PRIMARY KEY,
-  name VARCHAR(20)
+  id               SERIAL PRIMARY KEY NOT NULL,
+  business_name    VARCHAR(20),
+  user_id          INT,
+  business_address VARCHAR(50),
+  service_type_id  INT,
+  transport        BOOLEAN,
+  tools            BOOLEAN,
+  experience_id    INT,
+  cell_number INT,
+  FOREIGN KEY (service_type_id) REFERENCES service_type (id),
+  FOREIGN KEY (experience_id) REFERENCES years_exp (id),
+  FOREIGN KEY (user_id) REFERENCES user_dladle (id)
 );
 
-CREATE TABLE home_view_type
+/*lease*/
+CREATE TABLE lease
 (
-  id   SERIAL PRIMARY KEY,
-  name VARCHAR(20)
+  id           SERIAL PRIMARY KEY NOT NULL,
+  landlord_id  INT,
+  tenant_id    INT,
+  lease_date   DATE,
+  renewal_date DATE,
+  remark       VARCHAR(100),
+  FOREIGN KEY (landlord_id) REFERENCES landlord (id),
+  FOREIGN KEY (tenant_id) REFERENCES tenant (id)
 );
-
-CREATE TABLE years_exp
+/*service*/
+CREATE TABLE service
 (
-  id   SERIAL PRIMARY KEY,
-  name VARCHAR(20)
+  id              SERIAL PRIMARY KEY NOT NULL,
+  service_type_id INT,
+  vendor_id       INT,
+  FOREIGN KEY (service_type_id) REFERENCES service_type (id),
+  FOREIGN KEY (vendor_id) REFERENCES vendor (id)
 );
-
-CREATE UNIQUE INDEX user_dladle_email_uindex
-  ON user_dladle (emailid);
-ALTER TABLE user_dladle
-  ADD CONSTRAINT user_type_id FOREIGN KEY (user_type_id) REFERENCES user_type (id);
-ALTER TABLE landlord
-  ADD CONSTRAINT user_vendor_id FOREIGN KEY (user_id) REFERENCES user_dladle (id);
-ALTER TABLE tenant
-  ADD CONSTRAINT user_tenant_id FOREIGN KEY (user_id) REFERENCES user_dladle (id);
-ALTER TABLE tenant
-  ADD CONSTRAINT landlord_id FOREIGN KEY (landlord_id) REFERENCES landlord (id);
-ALTER TABLE tenant
-  ADD CONSTRAINT house_id FOREIGN KEY (house_id) REFERENCES house (id);
-ALTER TABLE vendor
-  ADD CONSTRAINT user_vendor_id FOREIGN KEY (user_id) REFERENCES user_dladle (id);
-ALTER TABLE service
-  ADD CONSTRAINT service_type_id FOREIGN KEY (service_type_id) REFERENCES service_type (id);
-ALTER TABLE service
-  ADD CONSTRAINT vendor_id FOREIGN KEY (vendor_id) REFERENCES vendor (id);
-ALTER TABLE house
-  ADD CONSTRAINT property_id FOREIGN KEY (property_id) REFERENCES property (id);
-ALTER TABLE property
-  ADD CONSTRAINT landlord_property_id FOREIGN KEY (landlord_id) REFERENCES landlord (id);
-ALTER TABLE lease
-  ADD CONSTRAINT landlord_lease_id FOREIGN KEY (landlord_id) REFERENCES landlord (id);
-ALTER TABLE lease
-  ADD CONSTRAINT tenant_lease_id FOREIGN KEY (tenant_id) REFERENCES tenant (id);
-ALTER TABLE rating_comments
-  ADD CONSTRAINT rating_id FOREIGN KEY (rating_id) REFERENCES rating (id);
-ALTER TABLE amenities
-  ADD CONSTRAINT amenities_type_id FOREIGN KEY (amenities_type_id) REFERENCES amenities_type (id);
-ALTER TABLE amenities
-  ADD CONSTRAINT house_id FOREIGN KEY (house_id) REFERENCES amenities_type (id)
-ALTER TABLE vendor
-  ADD FOREIGN KEY (service_type_id) REFERENCES service_type (id);
-
